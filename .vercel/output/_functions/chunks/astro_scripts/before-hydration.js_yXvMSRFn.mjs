fb2dabfa498e2410a8de251aa310a9186911005d
root <root@localhost.localdomain>
2026-08-04 01:07:58 +0530

    Remove prebuilt Vercel output

--- .vercel/output/_functions/chunks/astro_scripts/before-hydration.js_yXvMSRFn.mjs
+++
@@ -1,9 +0,0 @@
-import { n as runInjectionScript } from "../internal_wryDJWcy.mjs";
-//#region astro:scripts/before-hydration.js
-await runInjectionScript({ "sdkMetadata": {
-	"version": "4.0.5",
-	"name": "@clerk/astro",
-	"environment": "production"
-} });
-//#endregion
-export {};
--- .vercel/output/_functions/chunks/consts_kQkbns8j.mjs
+++
@@ -1,5 +0,0 @@
-//#region src/consts.ts
-var SITE_TITLE = "Astro Blog";
-var SITE_DESCRIPTION = "Welcome to my website!";
-//#endregion
-export { SITE_TITLE as n, SITE_DESCRIPTION as t };
